1725e693f8dfd881e93cfdf305a9a6277830c6f7
Bernardo Chiamolera <bchiamolera@furb.br>
2024-09-20 14:40:16 -0300

    fix/ Token toString

--- Compilador/src/compilador/analisadorLexico/Token.java
+++ Compilador/src/compilador/analisadorLexico/Token.java
@@ -27,9 +27,56 @@ public class Token
     {
         return position;
     }
+    
+    public final String getClasse() {
+        switch (this.id) {
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+            case 14:
+            case 15:
+                return "palavra reservada";
+            case 16:
+                return "identificador\t";
+            case 17:
+                return "constante_int\t";
+            case 18:
+                return "constante_float\t";
+            case 19:
+                return "constante_string";
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31:
+            case 32:
+            case 33:
+            case 34:
+            case 35:
+                return "simbolo especial";
+            default:
+                return "token desconhecido";
+        }
+    }
 
     public String toString()
     {
-        return id+" ( "+lexeme+" ) @ "+position;
+        return position + "\t" + getClasse() + "\t" + lexeme + "\n";
     };
 }
