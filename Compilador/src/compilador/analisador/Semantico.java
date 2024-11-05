e84b77e547d5f9f4d98a7f8187d6a92164dde872
Bernardo Chiamolera <bchiamolera@furb.br>
2024-11-05 08:53:01 -0300

    fix/ sintatico semantico 2

--- Compilador/src/compilador/analisador/Semantico.java
+++ Compilador/src/compilador/analisador/Semantico.java
@@ -4,6 +4,6 @@ public class Semantico implements Constants
 {
     public void executeAction(int action, Token token)	throws SemanticError
     {
-        System.out.println("A��o #"+action+", Token: "+token);
+        System.out.println("Acao #"+action+", Token: "+token);
     }	
 }
